89816064a38775ad7089b9390919b1a8ec07ea88
aminpirakand <amin.pirakand@yahoo.com>
2023-12-19 22:02:16 +0330

    create context and shop category

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import LoginSingup from './Pages/LoginSingup';
 import Cart from './Pages/Cart';
 import Nopages from './Pages/Nopages';
 import Footer from './Components/Footer/Footer';
+import men_banner from "./Components/Assets/banner_mens.png"
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
+
+
+
+
 function App() {
   return (
     <div>
@@ -15,9 +22,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/womens" element={<ShopCategory category="womens" />} />
-          <Route path="/mens" element={<ShopCategory category="mens" />} />
-          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid" />} />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
